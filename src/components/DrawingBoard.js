fbf0f01a201d58a633ea3514a5716da2d3a40a48
BanhMilos <448866426n@gmail.com>
2025-05-12 17:33:50 +0700

    Fix3

--- src/components/DrawingBoard.js
+++ src/components/DrawingBoard.js
@@ -2,7 +2,6 @@ import React, { useRef, useState, useEffect } from "react";
 import { Stage, Layer, Line, Rect, Circle } from "react-konva";
 import AppColors from "core/constants/AppColors";
 import AppImages from "core/constants/AppImages";
-import { clear } from "@testing-library/user-event/dist/clear";
 
 const DrawingBoard = ({ disableTool, showTooltip, socket, roomId, username }) => {
   const [lines, setLines] = useState([]);
@@ -84,7 +83,7 @@ const DrawingBoard = ({ disableTool, showTooltip, socket, roomId, username }) =>
       socket.off("drawing");
       socket.off("clearCanvas");
     };
-  }, []);
+  });
 
   const handleColorTooltip = (color) => {
     return `Current color: ${color}`;
